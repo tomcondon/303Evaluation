e3aacfd29aea3505e7cc735754d16f9e614e6a40
Thomas Condon <tomcondon@mac.com>
2019-05-23 18:06:26 -0600

    First commit

---
+++ 303Evaluation/AppModel.swift
@@ -0,0 +1,16 @@
+//
+//  AppModel.swift
+//  303Evaluation
+//
+//  Created by Thomas Condon on 5/23/19.
+//  Copyright © 2019 Niwot Software, LLC. All rights reserved.
+//
+
+import Foundation
+
+
+class AppModel {
+    static let sharedInstance
+    
+    var 
+}
---
+++ 303EvaluationTests/_03EvaluationTests.swift
@@ -0,0 +1,34 @@
+//
+//  _03EvaluationTests.swift
+//  303EvaluationTests
+//
+//  Created by Thomas Condon on 5/23/19.
+//  Copyright © 2019 Niwot Software, LLC. All rights reserved.
+//
+
+import XCTest
+@testable import _03Evaluation
+
+class _03EvaluationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
